b59cda091f6e5acf8c4fecbfdb5a154e9dd7ac21
ijazmohammed <mohammedijas619@gmail.com>
2019-03-10 13:43:58 +0530

    removed error file connections.php

--- includes/connections.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-  echo "Failed to connect to MySQL: "
-	require_once("config.php");
-	$mysqli = new mysqli_connect($server, $sqlid, $sqlpass, $dbase);
-	if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
-?>
